7b2ab7e794f6610ded06bdff8ed37c83bda0ab72
donghyeon0725 <ehdgus5015@gmail.com>
2021-04-25 15:45:22 +0900

    에라토스테네스의 채 + 피보나치 수열 문제

--- src/com/inflearn/two/Five.java
+++ src/com/inflearn/two/Five.java
@@ -3,11 +3,11 @@ package com.inflearn.two;
 import java.util.Scanner;
 
 /**
- * 에라토스 채
+ * 에라토스테네스 채
  * */
 public class Five {
     public int solution(int n) {
-        int answer = 0;
+        int count = 0;
 
         // n+1 크기 만큼 생성 1부터 n까지의 인덱스로 접근할 수 있음
         int[] nums = new int[n+1];
@@ -17,7 +17,7 @@ public class Five {
 
             // 1이면 이미 제외된 숫자
             if (nums[i] == 0) {
-                answer++;
+                count++;
                 for (int j = i; j<nums.length; j+=i) {
                     // i의 배수가 1이 아닐 경우만 1로 변경
                     if (nums[j] != 1) {
@@ -28,7 +28,7 @@ public class Five {
         }
 
 
-        return answer;
+        return count;
     }
 
     public static void main(String[] args) {
