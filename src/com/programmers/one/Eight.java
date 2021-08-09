64fe53582d64a94107f89fff0a3b5d26ebeed7f2
donghyeon0725 <ehdgus5015@gmail.com>
2021-08-09 12:23:51 +0900

    프로그래머스 문제 풀이 추가

---
+++ src/com/programmers/one/Eight.java
@@ -0,0 +1,118 @@
+package com.programmers.one;
+
+public class Eight {
+    public int solution(int m, int n, int[][] puddles) {
+
+        int[][] node = new int[n][m];
+
+        for (int p = 0; p<puddles.length; p++) {
+            int x = puddles[p][0]-1;
+            int y = puddles[p][1]-1;
+            node[y][x] = -1;
+        }
+
+        for (int x=0; x<m; x++)
+            if (node[0][x] != -1)
+                node[0][x] = 1;
+
+        for (int y=0; y<n; y++)
+            if (node[y][0] != -1)
+                node[y][0] = 1;
+
+        for (int y=1; y<n; y++)
+            for (int x=1; x<m; x++) {
+                if (node[y][x] != -1) {
+                    node[y][x] = (node[y-1][x] != - 1 ? node[y-1][x] : 0) + (node[y][x-1] != -1 ? node[y][x-1] : 0);
+                }
+            }
+
+        return node[n-1][m-1] % 1000000007;
+    }
+
+
+    /**
+     * 경로가 모두 막힌 경우를 고려해야 하기 때문에 1로 채우고 시작할 것이 아니라,
+     * 처음에만 1을 부여하고 채워가는 방법을 선택해야 합니다.
+     *
+     * 또한 데이터는 중복해서 들어가기 때문에 순식간에 값을 넘어버린다.
+     *
+     * */
+    public int solution1(int m, int n, int[][] puddles) {
+
+        long[][] node = new long[n][m];
+        node[0][0] = 1;
+
+        for (int p = 0; p<puddles.length; p++) {
+            int x = puddles[p][0]-1;
+            int y = puddles[p][1]-1;
+            node[y][x] = -1;
+        }
+
+
+        for (int y=0; y<n; y++)
+            for (int x=0; x<m; x++) {
+                if (x == 0 && y == 0)
+                    continue;
+
+                if (y == 0 && node[y][x] != -1)
+                    node[y][x] = (node[y][x-1] != -1 ? node[y][x-1] : 0);
+                else if (x == 0 && node[y][x] != -1)
+                    node[y][x] = (node[y-1][x] != - 1 ? node[y-1][x] : 0);
+                else if (node[y][x] != -1) {
+                    node[y][x] = (node[y-1][x] != - 1 ? node[y-1][x] : 0) + (node[y][x-1] != -1 ? node[y][x-1] : 0);
+                }
+            }
+
+        return (int)(node[n-1][m-1] % 1000000007);
+    }
+
+    /**
+     *
+     * 문제를 풀 때 좌 우 100 만 넘어가도
+     * 수가 기하급수적으로 커지기 때문에 1000000007 으로 나눈 값을 반환하라는 부분이 있다.
+     *
+     * 주의할 점은, 계산 중간 크기가 int 변수 최대 범위를 넘어가는 방지하기 위해 중간에 1000000007 으로 나누어 주어야 한다고 한다.
+     *
+     * 한편, 배열을 0으로 한번 감싸면 문제 풀이가 더 쉬워진다.
+     * ===================
+     * 집    1   1   1
+     * 1    0   1   2
+     * 1    1   2   4
+     * ==================
+     * 0    0    0   0   0
+     * 0   집    1   1   1
+     * 0    1    0   1   2
+     * 0    1    1   2   4
+     * ===================
+     * 이렇게 하고 계산을 하면 OutOfIndex Exception 을 위한 방어코드를 넣지 않아도 된다.
+     * */
+    public int solution2(int m, int n, int[][] puddles) {
+
+        long[][] node = new long[n+1][m+1];
+        boolean[][] isPuddles = new boolean[n+1][m+1];
+        node[1][1] = 1;
+
+        for (int p = 0; p<puddles.length; p++)
+            isPuddles[puddles[p][1]][puddles[p][0]] = true;
+
+        for (int y=1; y<=n; y++)
+            for (int x=1; x<=m; x++) {
+                if (x == 1 && y == 1)
+                    continue;
+
+                node[y][x] = ((!isPuddles[y-1][x] ? node[y-1][x] : 0) + (!isPuddles[y][x-1]? node[y][x-1] : 0)) % 1000000007;
+            }
+
+        return (int)node[n][m];
+    }
+
+
+    public static void main(String[] args) {
+        Eight eight = new Eight();
+
+        int m = 4;
+        int n = 3;
+        int[][] puddles = {{2, 2}};
+        System.out.println(eight.solution2(m, n, puddles));
+    }
+}
