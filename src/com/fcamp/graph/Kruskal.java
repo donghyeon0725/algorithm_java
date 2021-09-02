a1bf8b7aadece2d39023495a24cfe7d73f1aa8c6
donghyeon0725 <ehdgus5015@gmail.com>
2021-09-02 10:59:16 +0900

    문제 풀이

---
+++ src/com/fcamp/graph/Kruskal.java
@@ -0,0 +1,98 @@
+package com.fcamp.graph;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Kruskal {
+    static class DisjointSet {
+        private int[] parent;
+        private int[] rank;
+
+        // 초기화
+
+        public DisjointSet(int size) {
+            this.parent = new int[size];
+            this.rank = new int[size];
+
+            for (int i=0; i<parent.length; i++)
+                parent[i] = i;
+        }
+
+
+        // union
+        public void union(int a, int b) {
+            int pa = findParent(a);
+            int pb = findParent(b);
+
+
+            // 랭크가 같으면
+            if (rank[pa] == rank[pb]) {
+                parent[pa] = pb;
+                rank[pb]++;
+                return;
+            }
+
+            if (rank[pa] > rank[pb])
+                parent[pa] = pb;
+            else
+                parent[pb] = pa;
+        }
+
+        // find
+        public boolean find(int a, int b) {
+            return findParent(a) == findParent(b);
+        }
+
+        // getParent
+        public int findParent(int a) {
+            if (parent[a] == a) return a;
+            return findParent(parent[a]);
+        }
+    }
+
+    public Integer[][] solution(int[][] graph) {
+        List<Integer[]> result = new ArrayList<>();
+        Queue<Integer[]> edges = new LinkedList<>();
+        // 그래프 변환
+        for (int start = 0; start < graph.length; start++) {
+            for (int end = 0;  end <= start; end++) {
+                if (graph[start][end] != 0)
+                    edges.add(new Integer[]{graph[start][end], start, end});
+            }
+        }
+        edges = edges.stream().sorted((a, b) -> a[0] - b[0]).collect(Collectors.toCollection(LinkedList::new));
+
+        DisjointSet set = new DisjointSet(edges.size());
+
+        while (!edges.isEmpty()) {
+            Integer[] edge = edges.poll();
+
+            // 이어도 사이클을 이루지 않음
+            if (!set.find(edge[1], edge[2])) {
+                result.add(edge);
+                set.union(edge[1], edge[2]);
+            }
+        }
+
+        return result.toArray(new Integer[][]{});
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {
+                {0, 7, 0, 5, 0, 0, 0},
+                {7, 0, 8, 9, 7, 0, 0},
+                {0, 8, 0, 0, 5, 0, 0},
+                {5, 9, 0, 0, 7, 6, 0},
+                {0, 7, 5, 7, 0, 8, 9},
+                {0, 0, 0, 6, 8, 0, 11},
+                {0, 0, 0, 0, 9, 11, 0}
+        };
+
+        Kruskal kruskal = new Kruskal();
+        final Integer[][] solution = kruskal.solution(graph);
+
+
+        for (Integer[] s : solution)
+            System.out.println("길이 = " + s[0] +  ", " + (char)(65 + s[1]) + " <=> " + (char)(65 + s[2]));
+    }
+}
