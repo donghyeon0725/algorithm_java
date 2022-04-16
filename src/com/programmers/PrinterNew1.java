8dfe5c17696eb470fd9a030b2ead5d71f9ea1a48
김동현 <donghyeon@liveklass.com>
2022-04-16 16:02:49 +0900

    marathon

--- src/com/programmers/PrinterNew1.java
+++ src/com/programmers/PrinterNew1.java
@@ -1,11 +1,12 @@
 package com.programmers;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.regex.Pattern;
 
 public class PrinterNew1 {
-
     /**
      * 우선순위 배열을 정렬해서, 앞에서 부터 인덱스 차례대로 비교해 나가면 될 것 같음
      * 인덱스 값에 해당하는 문서가 나올 때까지 queue 를 poll & push 하면 됩니다.
@@ -13,7 +14,6 @@ public class PrinterNew1 {
      * Arrays.sort 를 사용하면 낮은 값이 앞에 온다는 점 주의해서 사용하기
      * */
     public int solution(int[] priorities, int location) {
-
         Queue<Integer> queue = new LinkedList<>();
         int targetCursor = location;
         int maxCursor = 0;
@@ -47,9 +47,67 @@ public class PrinterNew1 {
     }
 
     public static void main(String[] args) {
-        PrinterNew1 printer = new PrinterNew1();
-//        int[] priorities = {2, 1, 3, 2};
-        int[] priorities = {1, 1, 9, 1, 1, 1};
-        System.out.println(printer.solution(priorities, 0));
+
+        int n = 9;
+        int[][] wires = {{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}};
+//        int[][] wires = {{1,2},{3,4}};
+//        int[][] wires = {{1,2},{2,7},{3,4},{4,5},{6,7}};
+
+        PrinterNew1 printerNew1 = new PrinterNew1();
+
+        System.out.println(printerNew1.solution(n, wires));;
     }
+
+    public int solution(int n, int[][] wires) {
+
+        int min = Integer.MAX_VALUE;
+
+        for (int t=0; t<wires.length; t++) {
+            int[] dis = wires[t];
+            int[][] newWires = newArrWithRemove(wires, t);
+
+            int[] parent = new int[n];
+            int[] size = new int[n];
+
+            Arrays.fill(size, 1);
+            for (int i=0; i<parent.length; i++) {
+                parent[i] = i;
+            }
+
+            for (int[] wire : newWires) {
+                int rootL = parent[wire[0] - 1];
+                int rootR = parent[wire[1] - 1];
+                int tempL = rootL;
+
+                rootL = rootL < rootR ? rootL : rootR;
+                rootR = tempL < rootR ? rootR : tempL;
+
+                parent[rootL] = rootL;
+                parent[rootR] = rootL;
+                size[rootL] = size[rootL] + size[rootR];
+            }
+
+            int result = size[parent[dis[0] - 1]] - size[parent[dis[1] - 1]];
+            if (Math.abs(result) < min) {
+                min = Math.abs(result);
+            }
+        }
+
+        return min;
+    }
+
+    public int[][] newArrWithRemove(int[][] arr, int t) {
+        int[][] result = new int[arr.length - 1][2];
+
+        int index = 0;
+        for (int i=0; i<arr.length; i++) {
+            if (i != t) {
+                result[index] = arr[i];
+                index++;
+            }
+        }
+
+        return result;
+    }
+
 }
---
+++ src/com/programmers/four/Marathon.java
@@ -0,0 +1,36 @@
+package com.programmers.four;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Marathon {
+    public String solution(String[] participant, String[] completion) {
+
+        Map<String, Integer> map = new HashMap(participant.length);
+
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, Integer.valueOf(0)) + 1);
+        }
+
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+
+
+        Integer one = 1;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (one.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+
+        Marathon marathon = new Marathon();
+        System.out.println(marathon.solution(participant, completion));;
+    }
+}
